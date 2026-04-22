39bdde41ee76cc188e2a3c0732a2d2f185da6891
prapuldev <prapulb.dev@gmail.com>
2026-04-22 15:00:20 +0530

    modified: added * angular-16-client/src/app/services/tutorial.service.ts

--- angular-16-client/src/app/services/tutorial.service.ts
+++ angular-16-client/src/app/services/tutorial.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Tutorial } from '../models/tutorial.model';
 
-const baseUrl = 'http://102.210.148.235:8085/api/tutorials';
+const baseUrl = '/api/tutorials';
 
 @Injectable({
   providedIn: 'root',
